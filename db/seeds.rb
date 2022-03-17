39d0c67963283821f229014b66ceef91679b3956
Brenda <brendacassita@gmail.com>
2022-03-16 18:31:54 -0600

    hw

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Song.destroy_all
 
-Song.create(songname:'Come As You Are', artist:'Nirvana')
-Song.create(songname:'Jammin', artist:'Bob Marley')
+
 Song.create(songname:'KOD', artist: 'JCole')
 Song.create(songname:'Its Never Enough', artist:'Audiodub')
 Song.create(songname:'Pretty Little Fears', artist: 'Jcole and 6lack')
-Song.create(songname:'Youre Somebody else', artist:'Flora Cash')
 Song.create(songname:'Somebody Else', artist:'The 1975')
 
 puts "SONGS: #{Song.all.count}"
